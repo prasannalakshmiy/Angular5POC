d86e3578a17c8dcdc2f8a671c7c86cd2bad5a123
prasannalakshmiy <lakshmi.prasanna073@gmail.com>
2018-04-16 12:51:23 +0530

    binding

--- Angular5POC/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular5POC/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+recipes:Recipe[]=[
+  new Recipe('pasta','boil the pasta','https://images-na.ssl-images-amazon.com/images/G/01/aplusautomation/vendorimages/e98af2ff-e29c-4d92-915d-711412b470c3.jpg._CB528289739__SL300__.jpg'), new Recipe('hard-boiled-eggs','boil the eggs in a vessel','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTadj3oGoREEHdV2UKKUgsIkYZSkgPtWs9MjlsIPAsMXAnzDztI3g')];
 
   constructor() { }
 
--- Angular5POC/src/app/shopping-list/shopping-list.component.ts
+++ Angular5POC/src/app/shopping-list/shopping-list.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Ingredient} from '../shared/ingredient.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+ingredients :Ingredient[]= [new Ingredient('pasta',1),new Ingredient('eggs',6) ];
   constructor() { }
 
   ngOnInit() {
